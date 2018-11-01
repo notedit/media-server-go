c7e1eb8f60cba741188886a566cbbdcce2d47c8d
notedit <notedit@gmail.com>
2018-11-01 18:47:13 +0800

    build

---
+++ config.mk
@@ -0,0 +1,16 @@
+#################################
+# Config file
+##################################
+LOG				= yes
+DEBUG			= yes
+SANITIZE		= no
+STATIC			= yes
+STATIC_OPENSSL	= yes
+STATIC_LIBSRTP	= yes
+STATIC_LIBMP4	= yes
+OPENSSL_DIR		= /Users/xiang/Work/RTCEngine/media-server-go/external/openssl
+LIBSRTP_DIR		= /Users/xiang/Work/RTCEngine/media-server-go/external/libsrtp
+LIBMP4_DIR		= /Users/xiang/Work/RTCEngine/media-server-go/external/mp4v2
+VADWEBRTC		= no
+SRCDIR			= /Users/xiang/Work/RTCEngine/media-server-go/mediaserver
+IMAGEMAGICK		= no
---
+++ lib.go
@@ -0,0 +1,9 @@
+package mediaserver
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/external/libsrtp/include
+#cgo CFLAGS: -I${SRCDIR}/external/openssl/include
+#cgo CFLAGS: -I${SRCDIR}/external/mp4v2/include
+#cgo LDFLAGS: -L${SRCDIR}/mediaserver/bin/debug/libmediaserver.a
+*/
+import "C"
