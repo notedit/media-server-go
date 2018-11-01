08f436d08229d01c621f578996193a19232aea13
notedit <notedit@gmail.com>
2018-11-02 03:11:20 +0800

    it builds

--- lib.go
+++ lib.go
@@ -1,11 +1,15 @@
 package mediaserver
 
 /*
-#cgo CXXFLAGS: -I${SRCDIR}/external/libsrtp/include
-#cgo CXXFLAGS: -I${SRCDIR}/external/openssl/include
-#cgo CXXFLAGS: -I${SRCDIR}/external/mp4v2/include
-#cgo CXXFLAGS: -I${SRCDIR}/mediaserver/include
-#cgo CXXFLAGS: -I${SRCDIR}/mediaserver/src
-#cgo LDFLAGS: -l${SRCDIR}/mediaserver/bin/debug/libmediaserver.a -lstdc++
+#cgo CPPFLAGS: -I${SRCDIR}/external/libsrtp/include
+#cgo CPPFLAGS: -I${SRCDIR}/external/openssl/include
+#cgo CPPFLAGS: -I${SRCDIR}/external/mp4v2/include
+#cgo CPPFLAGS: -I${SRCDIR}/mediaserver/include
+#cgo LDFLAGS: -L${SRCDIR}/mediaserver/bin/debug/ -lmediaserver
+#cgo LDFLAGS: -L${SRCDIR}/external/libsrtp -lsrtp2
+#cgo LDFLAGS: -lpthread
+#cgo LDFLAGS: -L${SRCDIR}/external/openssl -lcrypto
+#cgo LDFLAGS: -L${SRCDIR}/external/openssl -lssl
+#cgo LDFLAGS: -L${SRCDIR}/external/mp4v2/.libs -lmp4v2
 */
 import "C"
