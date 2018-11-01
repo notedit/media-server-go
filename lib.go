f723a26c584fbfefb3baaba4efe0c573437228bb
notedit <notedit@gmail.com>
2018-11-01 19:05:38 +0800

    fix

--- lib.go
+++ lib.go
@@ -1,9 +1,7 @@
 package mediaserver
 
 /*
-#cgo CFLAGS: -I${SRCDIR}/external/libsrtp/include
-#cgo CFLAGS: -I${SRCDIR}/external/openssl/include
-#cgo CFLAGS: -I${SRCDIR}/external/mp4v2/include
+#cgo CFLAGS: -I${SRCDIR}/external/libsrtp/include -I/Users/xiang/Work/RTCEngine/media-server-go/external/openssl/include -I${SRCDIR}/external/mp4v2/include
 #cgo LDFLAGS: -L${SRCDIR}/mediaserver/bin/debug/libmediaserver.a
 */
 import "C"
