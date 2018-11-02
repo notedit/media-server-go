f86e7096b9aa6537025c05ae50c7e885ad023626
notedit <notedit@gmail.com>
2018-11-02 11:56:59 +0800

    it works

---
+++ endpoint.go
@@ -0,0 +1,42 @@
+package mediaserver
+
+type Endpoint struct {
+	ip          string
+	bundle      RTPBundleTransport
+	transports  map[int]*Transport
+	candidate   interface{}
+	fingerprint string
+}
+
+func NewEndpoint(ip string) *Endpoint {
+	bundle := NewRTPBundleTransport()
+	bundle.Init()
+	fingerprint := MediaServerGetFingerprint().ToString()
+	// candidate todo
+	return &Endpoint{
+		ip:          ip,
+		bundle:      bundle,
+		transports:  make(map[int]*Transport),
+		fingerprint: fingerprint,
+	}
+}
+
+func (e *Endpoint) CreateTransport() {
+
+}
+
+func (e *Endpoint) GetLocalCandidates() {
+
+}
+
+func (e *Endpoint) GetDTLSFingerprint() string {
+	return e.fingerprint
+}
+
+func (e *Endpoint) CreateOffer() {
+
+}
+
+func (e *Endpoint) Stop() {
+
+}
