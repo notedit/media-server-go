698c077557ab35f656f70cb13c03c8c266a96a35
notedit <notedit@gmail.com>
2018-11-07 01:20:18 +0800

    fix outgoing track

--- outgoingstreamtrack.go
+++ outgoingstreamtrack.go
@@ -3,11 +3,12 @@ package mediaserver
 import "github.com/chuckpreslar/emission"
 
 type OutgoingStreamTrack struct {
-	id     string
-	media  string
-	sender RTPSenderFacade
-	source RTPOutgoingSourceGroup
-	muted  bool
+	id         string
+	media      string
+	muted      bool
+	sender     RTPSenderFacade
+	source     RTPOutgoingSourceGroup
+	transpoder *Transponder
 	*emission.Emitter
 }
 
@@ -30,6 +31,7 @@ func newOutgoingStreamTrack(media string, id string, sender RTPSenderFacade, sou
 	track.Emitter = emission.NewEmitter()
 
 	// todo onremb callback
+
 	return track
 }
 
@@ -45,8 +47,13 @@ func (o *OutgoingStreamTrack) GetStats() {
 
 }
 
-func (o *OutgoingStreamTrack) GetSSRCs() {
+func (o *OutgoingStreamTrack) GetSSRCs() map[string]RTPOutgoingSource {
 
+	return map[string]RTPOutgoingSource{
+		"media": o.source.GetMedia(),
+		"rtx":   o.source.GetRtx(),
+		"fec":   o.source.GetFec(),
+	}
 }
 
 func (o *OutgoingStreamTrack) IsMuted() bool {
@@ -55,19 +62,73 @@ func (o *OutgoingStreamTrack) IsMuted() bool {
 
 func (o *OutgoingStreamTrack) Mute(muting bool) {
 
+	if o.transpoder != nil {
+		o.transpoder.Mute(muting)
+	}
+
+	if o.muted != muting {
+
+		o.muted = muting
+
+		o.EmitSync("muted", o.muted)
+	}
 }
 
 func (o *OutgoingStreamTrack) AttachTo(incomingTrack *IncomingStreamTrack) *Transponder {
 
-	return nil
+	// detach first
+	o.Detach()
+
+	// todo add remblistener
+	transponder := NewRTPStreamTransponderFacade(o.source, o.sender, nil)
+
+	o.transpoder = NewTransponder(transponder)
+
+	if o.muted {
+		o.transpoder.Mute(o.muted)
+	}
+
+	o.transpoder.SetIncomingTrack(incomingTrack)
+
+	o.transpoder.Once("stopped", o.onTransponderStopped)
+
+	return o.transpoder
 }
 
 func (o *OutgoingStreamTrack) Detach() {
 
+	if o.transpoder == nil {
+		return
+	}
+
+	o.transpoder.Off("stopped", o.onTransponderStopped)
+
+	o.transpoder.Stop()
+
+	o.transpoder = nil
 }
 
-// todo
+func (o *OutgoingStreamTrack) GetTransponder() *Transponder {
+
+	return o.transpoder
+}
 
 func (o *OutgoingStreamTrack) Stop() {
 
+	if o.sender == nil {
+		return
+	}
+
+	o.Detach()
+
+	o.EmitSync("stopped")
+
+	o.source = nil
+
+	o.sender = nil
+}
+
+func (o *OutgoingStreamTrack) onTransponderStopped() {
+
+	o.transpoder = nil
 }
--- transponder.go
+++ transponder.go
@@ -1,4 +1,24 @@
 package mediaserver
 
+import "github.com/chuckpreslar/emission"
+
 type Transponder struct {
+	*emission.Emitter
+}
+
+func NewTransponder(transponder RTPStreamTransponderFacade) *Transponder {
+
+	return nil
+}
+
+func (t *Transponder) Mute(muting bool) {
+
+}
+
+func (t *Transponder) SetIncomingTrack(incomingTrack *IncomingStreamTrack) {
+
+}
+
+func (t *Transponder) Stop() {
+
 }
