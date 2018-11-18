5a5ed37874821d271345176d3316d918977e3e25
notedit <notedit@gmail.com>
2018-11-18 15:13:50 +0800

    add more test

--- transport_test.go
+++ transport_test.go
@@ -2,16 +2,72 @@ package mediaserver
 
 import (
 	"testing"
+
+	"github.com/notedit/media-server-go/sdp"
 )
 
-func Test_Create(t *testing.T) {
+func Test_TransportCreate(t *testing.T) {
+
 	EnableLog(true)
+	endpoint := NewEndpoint("127.0.0.1")
 
-	// endpoint := NewEndpoint("127.0.0.1")
+	iceInfo := sdp.GenerateICEInfo(true)
+	dtlsInfo := sdp.NewDTLSInfo(sdp.SETUPACTPASS, "sha-256", "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F")
+	transport := endpoint.CreateTransportWithRemote(iceInfo, dtlsInfo, nil, false)
 
-	// iceInfo := sdp.GenerateICEInfo(true)
-	// dtlsInfo := sdp.NewDTLSInfo(sdp.SETUPACTIVE, "sha-256", "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F")
-	// transport := endpoint.CreateTransport(iceInfo, dtlsInfo)
-
+	if transport == nil {
+		t.Error("can not create transport")
+	}
 	t.Log("yes")
+}
+
+func Test_CreateIncomingTrack(t *testing.T) {
+
+	EnableLog(true)
+	endpoint := NewEndpoint("127.0.0.1")
+
+	iceInfo := sdp.GenerateICEInfo(true)
+	dtlsInfo := sdp.NewDTLSInfo(sdp.SETUPACTPASS, "sha-256", "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F")
+	transport := endpoint.CreateTransportWithRemote(iceInfo, dtlsInfo, nil, false)
+
+	incomingTrack := transport.CreateIncomingStreamTrack("audio", "audiotrack", map[string]uint{})
+
+	if incomingTrack.GetID() != "audiotrack" {
+		t.Error("create incoming track error")
+	}
+	t.Log("yes")
+}
+
+func Test_CreateOutgoingTrack(t *testing.T) {
+
+	EnableLog(true)
+	endpoint := NewEndpoint("127.0.0.1")
+
+	iceInfo := sdp.GenerateICEInfo(true)
+	dtlsInfo := sdp.NewDTLSInfo(sdp.SETUPACTPASS, "sha-256", "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F")
+	transport := endpoint.CreateTransportWithRemote(iceInfo, dtlsInfo, nil, false)
+
+	outgoingTrack := transport.CreateOutgoingStreamTrack("video", "videotrack", map[string]uint{})
+
+	if outgoingTrack.GetID() != "videotrack" {
+		t.Error("create outgoing track error")
+	}
+}
+
+func Test_TransportStop(t *testing.T) {
+
+	EnableLog(true)
+	endpoint := NewEndpoint("127.0.0.1")
+
+	iceInfo := sdp.GenerateICEInfo(true)
+	dtlsInfo := sdp.NewDTLSInfo(sdp.SETUPACTPASS, "sha-256", "F2:AA:0E:C3:22:59:5E:14:95:69:92:3D:13:B4:84:24:2C:C2:A2:C0:3E:FD:34:8E:5E:EA:6F:AF:52:CE:E6:0F")
+	transport := endpoint.CreateTransportWithRemote(iceInfo, dtlsInfo, nil, false)
+
+	transport.Once("stopped", func() {
+
+		t.Log("transport stopped")
+	})
+
+	endpoint.Stop()
+
 }
