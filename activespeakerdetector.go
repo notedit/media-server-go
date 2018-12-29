e708e169c942a1695bc906bf8b9b2fce5a520fec
notedit <notedit@gmail.com>
2018-12-29 11:53:23 +0800

    add active detector

--- activespeakerdetector.go
+++ activespeakerdetector.go
@@ -1,24 +1,22 @@
 package mediaserver
 
 import (
-	"github.com/chuckpreslar/emission"
 	native "github.com/notedit/media-server-go/wrapper"
 )
 
 type ActiveSpeakerDetector struct {
-	tracks   map[string]*IncomingStreamTrack
+	tracks   map[uint]*IncomingStreamTrack
 	detector native.ActiveSpeakerDetectorFacade
-	*emission.Emitter
 }
+
+// TODO, add active callback
 
 func NewActiveSpeakerDetector() *ActiveSpeakerDetector {
 
 	detector := &ActiveSpeakerDetector{}
-	detector.tracks = map[string]*IncomingStreamTrack{}
+	detector.tracks = map[uint]*IncomingStreamTrack{}
 	detector.detector = native.NewActiveSpeakerDetectorFacade()
-	detector.Emitter = emission.NewEmitter()
 
-	// todo
 	return detector
 }
 
@@ -28,12 +26,33 @@ func (a *ActiveSpeakerDetector) SetMinChangePeriod(minChangePeriod uint) {
 
 func (a *ActiveSpeakerDetector) AddSpeaker(track *IncomingStreamTrack) {
 
+	// We should make sure this source is the main source
+	source := track.GetFirstEncoding().GetSource()
+	if source == nil {
+		return
+	}
+	ssrc := source.GetMedia().GetSsrc()
+	a.tracks[ssrc] = track
+	a.detector.AddIncomingSourceGroup(source)
 }
 
 func (a *ActiveSpeakerDetector) RemoveSpeaker(track *IncomingStreamTrack) {
-
+	source := track.GetFirstEncoding().GetSource()
+	if source == nil {
+		return
+	}
+	delete(a.tracks, source.GetMedia().GetSsrc())
+	a.detector.RemoveIncomingSourceGroup(source)
 }
 
 func (a *ActiveSpeakerDetector) Stop() {
 
+	for _, track := range a.tracks {
+		source := track.GetFirstEncoding().GetSource()
+		a.detector.RemoveIncomingSourceGroup(source)
+	}
+
+	a.tracks = map[uint]*IncomingStreamTrack{}
+
+	native.DeleteActiveSpeakerDetectorFacade(a.detector)
 }
