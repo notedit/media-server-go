1dd980b908b4c78301374a325c766b109cf6ad27
notedit <notedit@gmail.com>
2019-06-24 11:06:50 +0800

    add some header

---
+++ include/media-server/include/VideoEncoderWorker.h
@@ -0,0 +1,71 @@
+/* 
+ * File:   VideoEncoderWorker.h
+ * Author: Sergio
+ *
+ * Created on 12 de agosto de 2014, 10:32
+ */
+
+#ifndef VIDEOENCODERWORKER_H
+#define	VIDEOENCODERWORKER_H
+
+#include <pthread.h>
+#include <set>
+#include "config.h"
+#include "codecs.h"
+#include "video.h"
+
+class VideoEncoderWorker
+{
+public:
+	VideoEncoderWorker();
+	virtual ~VideoEncoderWorker();
+
+	int Init(VideoInput *input);
+	int SetCodec(VideoCodec::Type codec,int mode,int fps,int bitrate,int intraPeriod,const Properties & properties);
+	int SetVideoCodec(VideoCodec::Type codec,int width, int height, int fps,int bitrate,int intraPeriod,const Properties & properties);
+	int End();
+
+	int  SetTemporalBitrateLimit(int bitrate);
+	bool AddListener(MediaFrame::Listener *listener);
+	bool RemoveListener(MediaFrame::Listener *listener);
+	void SendFPU();
+	
+	bool IsEncoding() { return encoding;	}
+	
+	int Start();
+	int Stop();
+	
+protected:
+	int Encode();
+
+private:
+	static void *startEncoding(void *par);
+
+private:
+	typedef std::set<MediaFrame::Listener*> Listeners;
+	
+private:
+	Listeners		listeners;
+	
+	VideoInput *input	= nullptr;
+	VideoCodec::Type codec  = VideoCodec::UNKNOWN;
+
+	int width		= 0;	
+	int height		= 0;	
+	int fps			= 0;
+	DWORD bitrate		= 0;
+	int intraPeriod		= 0;
+	int bitrateLimit	= 0;
+	int bitrateLimitCount	= 0;
+	Properties properties;
+
+	pthread_t	thread;
+	pthread_mutex_t mutex;
+	pthread_cond_t	cond;
+	bool	encoding	 = false;
+	bool	sendFPU		 = false;
+};
+
+
+#endif	/* VIDEOENCODERWORKER_H */
+
